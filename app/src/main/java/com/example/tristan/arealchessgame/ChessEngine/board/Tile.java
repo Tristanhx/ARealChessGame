132c9457f115bcb38fdf6e7f95a9ca9969192da1
Tristanhx <tristanhx@hotmail.nl>
2017-06-10 11:19:18 +0200

    added Tile class and subclasses

--- app/src/main/java/com/example/tristan/arealchessgame/ChessEngine/board/Tile.java
+++ app/src/main/java/com/example/tristan/arealchessgame/ChessEngine/board/Tile.java
@@ -1,8 +1,26 @@
 package com.example.tristan.arealchessgame.ChessEngine.board;
 
+import com.example.tristan.arealchessgame.ChessEngine.pieces.Piece;
+
 /**
  * Created by Tristan on 06/06/2017.
  */
 
-public class Tile {
+public abstract class Tile {
+
+    // Every Tile has an x and y Coordinate
+    int xCoordinate;
+    int yCoordinate;
+
+    Tile (int xCoordinate, int yCoordinate){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    // This returns whether or not a tile is occupied
+    // It is easier to ask a tile if it is occupied that to ask every piece where it is
+    public abstract boolean tileIsOccupied();
+
+    // Returns an occupying piece
+    public abstract Piece getPiece();
 }
---
+++ app/src/main/java/com/example/tristan/arealchessgame/ChessEngine/board/TileEmpty.java
@@ -0,0 +1,24 @@
+package com.example.tristan.arealchessgame.ChessEngine.board;
+
+import com.example.tristan.arealchessgame.ChessEngine.pieces.Piece;
+
+/**
+ * Created by Tristan on 10/06/2017.
+ */
+
+public class TileEmpty extends Tile {
+
+    TileEmpty(int xCoordinate, int yCoordinate) {
+        super(xCoordinate, yCoordinate);
+    }
+
+    @Override
+    public boolean tileIsOccupied() {
+        return false;
+    }
+
+    @Override
+    public Piece getPiece() {
+        return null;
+    }
+}
