5b9615a7479e8b3e99ee55ae4d7126d7f23809e0
Tristanhx <tristanhx@hotmail.nl>
2017-06-10 11:43:03 +0200

    made a map of tiles

--- app/src/main/java/com/example/tristan/arealchessgame/ChessEngine/board/Tile.java
+++ app/src/main/java/com/example/tristan/arealchessgame/ChessEngine/board/Tile.java
@@ -2,6 +2,9 @@ package com.example.tristan.arealchessgame.ChessEngine.board;
 
 import com.example.tristan.arealchessgame.ChessEngine.pieces.Piece;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Tristan on 06/06/2017.
  */
@@ -11,6 +14,19 @@ public abstract class Tile {
     // Every Tile has an x and y Coordinate
     int xCoordinate;
     int yCoordinate;
+    
+    Map<Integer, TileEmpty> EMPTY_TILES = createAllEmptyTiles();
+
+    private Map<Integer,TileEmpty> createAllEmptyTiles() {
+        Map<Integer, TileEmpty> emptyTileMap = new HashMap<>();
+
+        for (int i = 0 ; i < 8 ; i++){
+            for (int j = 0 ; j < 8 ; j++){
+                emptyTileMap.put(i*j, new TileEmpty(i, j));
+            }
+        }
+        return emptyTileMap;
+    }
 
     Tile (int xCoordinate, int yCoordinate){
         this.xCoordinate = xCoordinate;
