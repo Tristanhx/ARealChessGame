f9efda916360bfcfbf45dadb6a3694ee13968977
Tristanhx <tristanhx@hotmail.nl>
2017-06-22 23:16:23 +0200

    Connecting AI 3

--- app/src/main/java/com/example/tristan/arealchessgame/GameChanger.java
+++ app/src/main/java/com/example/tristan/arealchessgame/GameChanger.java
@@ -1,16 +1,19 @@
 package com.example.tristan.arealchessgame;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
-import com.example.tristan.arealchessgame.chess_engine.Alliance;
 import com.example.tristan.arealchessgame.chess_engine.Setup;
 import com.example.tristan.arealchessgame.chess_engine.board.Board;
+import com.example.tristan.arealchessgame.chess_engine.move.Move;
+import com.example.tristan.arealchessgame.chess_engine.player.AlternateBoard;
 import com.example.tristan.arealchessgame.chess_engine.player.Player;
-import com.example.tristan.arealchessgame.chess_engine.player.Type;
 import com.example.tristan.arealchessgame.chess_engine.player.computer_player.MiniMax;
+import com.example.tristan.arealchessgame.chess_engine.player.computer_player.Strategy;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by trist on 6/21/2017.
@@ -51,5 +54,58 @@ public class GameChanger extends Observable {
     public void moveUpdate(final Type type){
         setChanged();
         notifyObservers(type);
+    }
+
+    public enum Type {
+        HUMAN,
+        COMPUTER
+    }
+
+    public class TurnWatcher implements Observer {
+
+
+        @Override
+        public void update(final Observable o, final Object arg) {
+            if (GameChanger.getInstance().getSetup().isComputer(Board.getInstance().getCurrentPlayer())
+                    && !Board.getInstance().getCurrentPlayer().checkMate() && !Board.getInstance().getCurrentPlayer().staleMate()){
+                final Thinker thinker = new Thinker();
+                thinker.execute();
+            }
+        }
+    }
+
+    class Thinker extends AsyncTask<Move, String, Move> {
+
+
+        public Thinker(){
+
+        }
+
+        @Override
+        protected Move doInBackground(Move... params) {
+
+            final Strategy miniMax = new MiniMax(2);
+            final Move bestMove = miniMax.execute(Board.getInstance());
+
+            return bestMove;
+        }
+
+
+        @Override
+        public void onPostExecute(Move move){
+
+            try {
+                final Move bestMove = get();
+                final AlternateBoard newBoard = Board.getInstance().getCurrentPlayer().makeMove(bestMove);
+                Board.instance = newBoard.getBoard();
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/tristan/arealchessgame/chess_engine/Setup.java
+++ app/src/main/java/com/example/tristan/arealchessgame/chess_engine/Setup.java
@@ -4,11 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import com.example.tristan.arealchessgame.GameActivity;
-import com.example.tristan.arealchessgame.SettingsActivity;
-import com.example.tristan.arealchessgame.SettingsFragment;
+import com.example.tristan.arealchessgame.GameChanger;
 import com.example.tristan.arealchessgame.chess_engine.player.Player;
-import com.example.tristan.arealchessgame.chess_engine.player.Type;
 
 /**
  * Created by Tristan on 22/06/2017.
@@ -16,8 +13,8 @@ import com.example.tristan.arealchessgame.chess_engine.player.Type;
 
 public class Setup {
 
-    private Type whiteType;
-    private Type blackType;
+    private GameChanger.Type whiteType;
+    private GameChanger.Type blackType;
     SharedPreferences preferences;
     String HvH;
     String CvC;
@@ -25,23 +22,23 @@ public class Setup {
         this.preferences = PreferenceManager.getDefaultSharedPreferences(context);
         HvH = preferences.getString("HvH", null);
         CvC = preferences.getString("CvC", null);
-        this.whiteType = CvC.equals("true") ? Type.COMPUTER : Type.HUMAN;
-        this.blackType = HvH.equals("true") ? Type.HUMAN : Type.COMPUTER;
+        this.whiteType = CvC.equals("true") ? GameChanger.Type.COMPUTER : GameChanger.Type.HUMAN;
+        this.blackType = HvH.equals("true") ? GameChanger.Type.HUMAN : GameChanger.Type.COMPUTER;
 
     }
 
     public boolean isComputer(final Player player){
         if (player.getAlliance() == Alliance.WHITE){
-            return getWhiteType() == Type.COMPUTER;
+            return getWhiteType() == GameChanger.Type.COMPUTER;
         }
-        return getBlackType() == Type.COMPUTER;
+        return getBlackType() == GameChanger.Type.COMPUTER;
     }
 
-    Type getWhiteType(){
+    GameChanger.Type getWhiteType(){
         return this.whiteType;
     }
 
-    Type getBlackType(){
+    GameChanger.Type getBlackType(){
         return this.blackType;
     }
 }
